7f7df11f1b6e5db4a62e48182ed599e1b5ffa844
chibuzo <uchibuzo1@gmail.com>
2026-02-21 00:10:40 +0100

    removed categories and author

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,15 +1,7 @@
 import { blogPost } from "./blogPost";
 import comment from "./comments";
-import author from "./authorType";
-import category from "./category";
 
 import { blockContentType } from "./blockContentType";
 
 // Wrap the array in an object with a 'types' property
-export const schemaTypes = [
-  blogPost,
-  comment,
-  author,
-  category,
-  blockContentType,
-];
+export const schemaTypes = [blogPost, comment, blockContentType];
